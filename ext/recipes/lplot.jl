5fcbbfa42ac8c41335751c31516f878e961a9875
Felix Hagemann <hagemann@mpp.mpg.de>
2025-02-20 06:34:26 +0100

    Add `lplot!` for histogram fits with custom components

--- ext/recipes/lplot.jl
+++ ext/recipes/lplot.jl
@@ -58,3 +58,77 @@ function LegendMakie.lplot!(
     
     fig
 end
+
+
+function LegendMakie.lplot!(
+        report::NamedTuple{(:v, :h, :f_fit, :f_components, :gof)};
+        xlabel = "", ylabel = "Counts / bin",
+        title::AbstractString = "", legend_position = :lt,
+        xlims = extrema(first(report.h.edges)), ylims = (0.9,maximum(report.h.weights) * 2),
+        show_label::Bool = true, show_components::Bool = true, yticks = Makie.automatic,
+        watermark::Bool = true, show_residuals::Bool = true, col::Int = 1, kwargs...
+    )
+
+    fig = Makie.current_figure()
+
+    # create plot
+    g = Makie.GridLayout(fig[1,col])
+    ax = Makie.Axis(g[1,1], 
+        title = title, titlefont = :bold, titlesize = 16pt, 
+        xlabel = xlabel, ylabel = ylabel, yticks = yticks,
+        limits = (xlims, ylims),
+        yscale = Makie.log10
+    )
+
+    Makie.hist!(ax, StatsBase.midpoints(first(report.h.edges)), weights = report.h.weights, bins = first(report.h.edges), color = LegendMakie.DiamondGrey, label = "Data")
+    Makie.lines!(range(xlims..., length = 1000), x -> report.f_fit(x) * step(first(report.h.edges)), color = :black, label = ifelse(show_label, "Best Fit" * (!isempty(report.gof) ? " (p = $(round(report.gof.pvalue, digits=2)))" : ""), ""))
+                
+    if show_components
+        for (idx, component) in enumerate(keys(report.f_components.funcs))
+            Makie.lines!(
+                range(extrema(first(report.h.edges))..., length = 1000), 
+                x -> report.f_components.funcs[component](x) * step(first(report.h.edges)), 
+                color = report.f_components.colors[component], 
+                label = ifelse(show_label, report.f_components.labels[component], ""),
+                linestyle = report.f_components.linestyles[component],
+                linewidth = 4
+            )
+        end
+    end
+
+    if legend_position != :none 
+        Makie.axislegend(ax, position = legend_position)
+    end
+
+    if !isempty(report.gof) && show_residuals
+
+        ax.xticklabelsize = 0
+        ax.xticksize = 0
+        ax.xlabel = ""
+
+        ax2 = Makie.Axis(g[2,1],
+                xlabel = xlabel, ylabel = "Residuals (σ)",
+                yticks = -3:3:3, limits = (xlims,(-5,5))
+            )
+        LegendMakie.residualplot!(ax2, (x = StatsBase.midpoints(first(report.h.edges)), residuals_norm = report.gof.residuals_norm))
+
+        Makie.linkxaxes!(ax, ax2)
+        Makie.rowgap!(g, 0)
+        Makie.rowsize!(g, 1, Makie.Auto(3))
+
+        # align ylabels
+        yspace = maximum(Makie.tight_yticklabel_spacing!, (ax, ax2))
+        ax.yticklabelspace = yspace
+        ax2.yticklabelspace = yspace
+    end
+
+    all = Makie.Axis(g[:,:])
+    Makie.hidedecorations!(all)
+    Makie.hidespines!(all)
+    Makie.current_axis!(all)
+
+    # add watermarks
+    watermark && LegendMakie.add_watermarks!(; kwargs...)
+
+    fig
+end
