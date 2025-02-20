6d0c5bcf8f814cb0c1fdfe59adf7c8a950e27082
Felix Hagemann <hagemann@mpp.mpg.de>
2025-02-20 03:04:59 +0100

    Add tests for Juleana/LEGEND watermarks

--- test/test_lplot.jl
+++ test/test_lplot.jl
@@ -1,10 +1,23 @@
 # This file is a part of LegendMakie.jl, licensed under the MIT License (MIT).
 
 using LegendMakie
-using Test
-
-# load Makie extension
 using Makie
 
+using Test
+
 @testset "lplot" begin
+    @testset "Test watermarks" begin
+
+        fig = Figure()
+
+        # test default watermark
+        ax = Axis(fig[1,1])
+        hist!(ax, randn(10000))
+        @test_nowarn LegendMakie.add_watermarks!()
+
+        # test alternative watermark
+        ax2 = Axis(fig[1,2])
+        hist!(ax2, randn(10000))
+        @test_nowarn LegendMakie.add_watermarks!(legend_logo = true, position = "outer top", preliminary = false)
+    end
 end
