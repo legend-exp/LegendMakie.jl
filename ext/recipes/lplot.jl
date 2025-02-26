70f772398d0eeb287901de4d7942a95bc9c2053a
Felix Hagemann <hagemann@mpp.mpg.de>
2025-02-26 19:22:56 +0100

    Add `@info` to fallback method of `lplot!`

--- ext/recipes/lplot.jl
+++ ext/recipes/lplot.jl
@@ -367,6 +367,8 @@ end
 # fallback method: use Makie.plot!
 function LegendMakie.lplot!(args...; watermark::Bool = false, kwargs...)
 
+    @info "No `LegendMakie` plot recipe found for this set of arguments. Using `Makie.plot!`"
+
     fig = Makie.current_figure()
     ax = isnothing(Makie.current_axis()) ? Makie.Axis(fig[1,1]) : Makie.current_axis()
 
--- test/test_lplot.jl
+++ test/test_lplot.jl
@@ -23,7 +23,7 @@ using Test
 
         # test default watermark
         ax = Axis(fig[1,1])
-        @test_nowarn lplot!(StatsBase.fit(StatsBase.Histogram, randn(10000)))
+        lplot!(StatsBase.fit(StatsBase.Histogram, randn(10000)))
         @test_nowarn LegendMakie.add_watermarks!()
 
         # test alternative watermark
