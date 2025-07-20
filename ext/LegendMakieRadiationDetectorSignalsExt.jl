8654c27c66995da050dcf7e48a1225b87a53e378
Felix Hagemann <hagemann@mpp.mpg.de>
2025-07-20 02:15:13 -0700

    Add more attribute defaults to `waveformplot!`

--- ext/LegendMakieRadiationDetectorSignalsExt.jl
+++ ext/LegendMakieRadiationDetectorSignalsExt.jl
@@ -9,17 +9,21 @@ module LegendMakieRadiationDetectorSignalsExt
 
     import LegendMakie: waveformplot!
 
-    Makie.@recipe(WaveformPlot, wf) do scene
-        Makie.Attributes(
-            linewidth = 1,
-            label = nothing,
-        )
+    Makie.@recipe WaveformPlot (wf,) begin 
+        cycle = [:color]
+        color = @inherit linecolor
+        joinstyle = @inherit joinstyle
+        linecap = @inherit linecap
+        alpha = 1.0
+        linewidth = 1
+        linestyle = nothing
+        label = nothing
     end
         
     function Makie.plot!(p::WaveformPlot{<:Tuple{<:RadiationDetectorSignals.RDWaveform}})
         wf = p.wf[]
         attr = Makie.Attributes(p)
-        Makie.lines!(p, attr, wf.time, wf.signal, label = p.label)
+        Makie.lines!(p, attr, wf.time, wf.signal)
         p
     end
 
